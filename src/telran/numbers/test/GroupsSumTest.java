e51f74a73a9597c70a1066beef37d58e199bc4fc
Yuragranovsky <yuragranovsky@gmail.com>
2024-08-05 15:44:45 +0300

    implementation of the method generating two dimensional array

--- src/telran/numbers/test/GroupsSumTest.java
+++ src/telran/numbers/test/GroupsSumTest.java
@@ -2,11 +2,14 @@ package telran.numbers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import telran.numbers.*;
 class GroupsSumTest {
-private static final int N_GROUPS = 1000;
-private static final int GROUP_LENGTH = 1000;
+private static final int N_GROUPS = 1000000;
+private static final int GROUP_LENGTH = 100;
 int[][] groups = {
 		{1,2},
 		{3,4},
@@ -19,9 +22,9 @@ int[][] largeGroups = getLargeGroups(N_GROUPS, GROUP_LENGTH);
 		assertEquals(21, gs.getSum());
 	}
 	private int[][] getLargeGroups(int nGroups, int groupLength) {
-		// TODO creating random two dimensional array 
-		// using method generate of Stream
-		return null;
+		
+		return Stream.generate(() -> new Random().ints(groupLength).toArray())
+				.limit(nGroups).toArray(int[][]::new);
 	}
 	@Test
 	void goupsSumThreadPoolTest() {
